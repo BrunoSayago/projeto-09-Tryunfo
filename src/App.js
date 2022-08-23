24d05acf93e7a72635fd552ce7a25e7d6ca1417d
Bruno Sayago <brunossgripp@gmail.com>
2022-08-23 17:25:07 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -21,6 +21,24 @@ class App extends React.Component {
     this.setState({ [name]: value });
   };
 
+  verificaAttr = (attr1, attr2, attr3) => {
+    const a1 = parseInt(attr1, 10);
+    const a2 = parseInt(attr2, 10);
+    const a3 = parseInt(attr3, 10);
+    const limitInd = 90;
+    const condA1 = (limitInd >= a1 && a1 >= 0);
+    const condA2 = (limitInd >= a2 && a2 >= 0);
+    const condA3 = (limitInd >= a3 && a3 >= 0);
+    const limitTot = 210;
+    return (condA1
+      && condA2 && condA3 && (a1 + a2 + a3) <= limitTot);
+  };
+
+  verificaStrings = (name, description, image, rarity) => (name !== ''
+    && description !== '' && image !== '' && rarity !== '');
+
+  verificaBotaoSalvar = (bool1, bool2) => bool1 && bool2;
+
   vaidom = () => 2;
 
   render() {
@@ -35,6 +53,10 @@ class App extends React.Component {
       cardTrunfo,
     } = this.state;
 
+    const bool1 = this.verificaStrings(cardName, cardDescription, cardImage, cardRare);
+
+    const bool2 = this.verificaAttr(cardAttr1, cardAttr2, cardAttr3);
+
     return (
       <div>
         <h1>Tryunfo</h1>
@@ -48,7 +70,7 @@ class App extends React.Component {
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
           //   hasTrunfo,
-          isSaveButtonDisabled
+          isSaveButtonDisabled={ !this.verificaBotaoSalvar(bool1, bool2) }
           onInputChange={ this.handleChange }
           onSaveButtonClick={ this.vaidom }
         />
